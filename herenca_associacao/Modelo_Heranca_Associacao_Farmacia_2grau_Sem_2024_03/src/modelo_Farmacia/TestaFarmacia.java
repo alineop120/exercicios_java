78c438c47e7743515cad26d2ec42ef97f0ccf4d3
Aline Pinho <134012132+alineop120@users.noreply.github.com>
2024-09-16 15:45:45 -0300

    Atualizar TestaFarmacia

--- herenca_associacao/Modelo_Heranca_Associacao_Farmacia_2grau_Sem_2024_03/src/modelo_Farmacia/TestaFarmacia.java
+++ herenca_associacao/Modelo_Heranca_Associacao_Farmacia_2grau_Sem_2024_03/src/modelo_Farmacia/TestaFarmacia.java
@@ -3,17 +3,17 @@ package modelo_Farmacia;
 public class TestaFarmacia {
 
     public static void main(String[] args) {
-        Compra c = new Compra(5, "20/09", 50.4, 
+        Compra c = new Compra(5, "20/09", 106.8, 
                 new Pagamento("a vista"),
-                new Cliente("744.851.484-16", "Roberta", "04/06/1950",
-                        new Pedido(int numero, Double valor, 
+                new Cliente("744.851.484-16", "Roberta", "04/06/1950", 
+                        new Pedido(56, 50.90, 
                                 new Atendente(45562, "Ana", 1500.9, 50), 
-                                new Perfumaria("456987", "Gabriel", 15.01,
-                                        new Fornecedor("45321", "Fabiana", "Rua"))),
-                        new Receita("02/10", 40.5,
-                                new Medicamento("456987", "nome", 50.4,
-                                        new Fornecedor("cnpj", "nome", "endereco"), "tarja", "valor"),
-                                new Farmaceutico(45, "Ana", 5000.4, "Medico")));        
+                                new Perfumaria("456987", "Gabriel", 15.01, 
+                                        new Fornecedor("45321", "Fabiana", "Rua 9"), 50.9, "prateleira 10")), 
+                        new Receita("02/10", 40.5, 
+                                new Medicamento("456987", "nome", 50.4, 
+                                        new Fornecedor("45321", "Fabiana", "Rua 9"), "tarja", "doenca"), 
+                                new Farmaceutico(56987, "Adriana", 5000.7, "Medica"))));        
         System.out.println(c);
     }
     
